a2b6a9d36aaa7ca096361562140859a686607030
Jacob Bradley <jtbradley1@gmail.com>
2023-03-04 00:25:16 -0500

    chore(deps): add `sitemap` integration
    `pnpm astro add sitemap`

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import { defineConfig } from 'astro/config';
 import vue from "@astrojs/vue";
 
 // https://astro.build/config
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [vue()]
+  integrations: [vue(), sitemap()]
 });
