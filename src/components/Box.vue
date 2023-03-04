9ad384b367d884dcdcfeca64ac6a7337aaaf5804
Jacob Bradley <jtbradley1@gmail.com>
2023-03-04 15:50:29 -0500

    feat: add animated `<Box>` component

---
+++ src/components/Box.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="box"></div>
+</template>
+
+  <script setup lang="ts">
+import anime from 'animejs';
+import { onMounted } from 'vue';
+
+onMounted(() => {
+  anime({
+        targets: '.box',
+        translateX: 250,
+        rotate: '1turn',
+        duration: 2000
+      });
+})
+</script>
+
+<style>
+  .box {
+    width: 100px;
+    height: 100px;
+    background-color: red;
+  }
+</style>
