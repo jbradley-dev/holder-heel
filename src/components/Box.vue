f35a8f695a72558a55133a9e0962d9bf5085990b
Jacob Bradley <jtbradley1@gmail.com>
2023-03-05 00:51:31 -0500

    style(eslint): `lint:fix`

--- src/components/Box.vue
+++ src/components/Box.vue
@@ -1,17 +1,17 @@
-<template>
-  <div class="box w-24 h-24 bg-red-500"></div>
-</template>
-
-  <script setup lang="ts">
-import anime from 'animejs';
-import { onMounted } from 'vue';
+<script setup lang="ts">
+import anime from 'animejs'
+import { onMounted } from 'vue'
 
 onMounted(() => {
   anime({
-        targets: '.box',
-        translateX: 250,
-        rotate: '1turn',
-        duration: 2000
-      });
+    targets: '.box',
+    translateX: 250,
+    rotate: '1turn',
+    duration: 2000,
+  })
 })
 </script>
+
+<template>
+  <div class="box w-24 h-24 bg-red-500" />
+</template>
--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import { ref } from 'vue'
 
 const count = ref<number>(0)
 </script>
 
 <template>
-<div id="app">
+  <div id="app">
     <button class="bg-red-500" @click="count++">
       Count is: {{ count }}
     </button>
